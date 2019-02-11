b50a25709351afb90ede87d9e19633f52c75d924
Torsten <tw@mitscom.de>
2019-02-11 10:44:41 +0100

    Initial & Login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,9 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="default")
      */
     public function index()
+    {
+        return $this->redirectToRoute('panel');
+    }
+
+    /**
+     * @Route("/panel", name="panel")
+     */
+    public function panel()
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',

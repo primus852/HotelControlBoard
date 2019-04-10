89510674b446f0d52b2d489a8026c98d991e1076
Torsten <tw@mitscom.de>
2019-04-10 09:14:55 +0200

    Fixed Calendar Bug

--- src/Util/Helper/Helper.php
+++ src/Util/Helper/Helper.php
@@ -33,7 +33,9 @@ class Helper
     /**
      * @param int $month
      * @param int $year
+     * @param ObjectManager $em
      * @return string
+     * @throws HelperException
      */
     public static function generate_calendar(int $month, int $year, ObjectManager $em)
     {
@@ -48,12 +50,12 @@ class Helper
         $calendar = '<table class="calendar"><tr><td colspan="7" class="calendar-month">' . $now->format('M') . '</td></tr>';
 
         /* table headings */
-        $headings = array('Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun');
+        $headings = array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
         $calendar .= '<tr class="calendar-row"><th class="calendar-day-head">' . implode('</th><th class="calendar-day-head">', $headings) . '</th></tr>';
 
         /* days and weeks vars now ... */
         $running_day = date('w', mktime(0, 0, 0, $month, 0, $year));
-        $days_in_month = date('t', mktime(0, 0, 0, $month, 0, $year));
+        $days_in_month = cal_days_in_month(CAL_GREGORIAN, $month, $year);
         $days_in_this_week = 1;
         $day_counter = 0;
 
@@ -61,13 +63,13 @@ class Helper
         $calendar .= '<tr class="calendar-row">';
 
         /* print "blank" days until the first of the current week */
-        for ($x = 0; $x < $running_day; $x++):
+        for ($x = 0; $x < $running_day; $x++){
             $calendar .= '<td class="calendar-day-np"> </td>';
             $days_in_this_week++;
-        endfor;
+        }
 
         /* keep going with days.... */
-        for ($list_day = 1; $list_day <= $days_in_month; $list_day++):
+        for ($list_day = 1; $list_day <= $days_in_month; $list_day++){
 
             try {
                 $today = DateTime::createFromFormat('Y-m-d', $year . '-' . $month . '-' . $list_day);
@@ -93,7 +95,7 @@ class Helper
                 }
             }
 
-            $calendar .= '<td class="calendar-day" style="background: ' . $color . ';color:'.$fontColor.';">';
+            $calendar .= '<td class="calendar-day" style="background: ' . $color . ';color:' . $fontColor . ';">';
             /* add in the day number */
             $calendar .= '<div class="day-number ' . $cxl . '">' . $list_day . '</div>';
 
@@ -111,14 +113,14 @@ class Helper
             $days_in_this_week++;
             $running_day++;
             $day_counter++;
-        endfor;
+        }
 
         /* finish the rest of the days in the week */
-        if ($days_in_this_week < 8):
-            for ($x = 1; $x <= (8 - $days_in_this_week); $x++):
+        if ($days_in_this_week < 8){
+            for ($x = 1; $x <= (8 - $days_in_this_week); $x++){
                 $calendar .= '<td class="calendar-day-np"> </td>';
-            endfor;
-        endif;
+            }
+        }
 
         /* final row */
         $calendar .= '</tr>';
